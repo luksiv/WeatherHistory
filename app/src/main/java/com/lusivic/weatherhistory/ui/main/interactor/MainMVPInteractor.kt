2759ac81775590ea036db247b0794f5ef72ea107
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-27 21:13:44 +0200

    Add new methods to MainInteractor interface

--- app/src/main/java/com/lusivic/weatherhistory/ui/main/interactor/MainMVPInteractor.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/interactor/MainMVPInteractor.kt
@@ -2,8 +2,11 @@ package com.lusivic.weatherhistory.ui.main.interactor
 
 import com.lusivic.weatherhistory.data.db.weatherReport.WeatherReport
 import com.lusivic.weatherhistory.ui.base.interactor.MVPInteractor
+import io.reactivex.Single
 
 interface MainMVPInteractor : MVPInteractor {
 
-    fun getCurrentWeatherReport(): WeatherReport
+    fun getCurrentWeatherReport(): Single<WeatherReport>
+    fun insertWeatherReport(weatherReport: WeatherReport)
+    fun getWeatherReportHistory(): Single<List<WeatherReport>>
 }
