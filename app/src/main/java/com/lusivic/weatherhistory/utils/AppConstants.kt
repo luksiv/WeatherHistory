d425991c8cb667ce0985d03b4ce9169276843976
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-27 17:08:46 +0200

    Remove OpenWeatherAPI key from app constants

--- app/src/main/java/com/lusivic/weatherhistory/utils/AppConstants.kt
+++ app/src/main/java/com/lusivic/weatherhistory/utils/AppConstants.kt
@@ -1,6 +1,7 @@
 package com.lusivic.weatherhistory.utils
 
 object AppConstants {
-    internal val OpenWeatherApiKey = "58cab431b7875800d819008f251aa7b2"
+
+    internal val APP_DB_NAME = "luksivic_weather.db"
 
 }
