6330a6537361388f85aa05065e7e520c23e3bec5
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-27 16:20:38 +0200

    Create MainPresenter interface

---
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/presenter/MainMVPPresenter.kt
@@ -0,0 +1,12 @@
+package com.lusivic.weatherhistory.ui.main.presenter
+
+import com.lusivic.weatherhistory.ui.base.presenter.MVPPresenter
+import com.lusivic.weatherhistory.ui.main.interactor.MainMVPInteractor
+import com.lusivic.weatherhistory.ui.main.view.MainMVPView
+
+interface MainMVPPresenter<V : MainMVPView, I : MainMVPInteractor> : MVPPresenter<V, I> {
+    fun getLatestWeatherReport(): Boolean?
+    fun onHistoryClick(): Unit?
+    fun onRefreshClick(): Unit?
+    fun onSubmitClick(): Unit?
+}
