7ca4c88909fa15ca7f1ce45e3c032ac3afffd544
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-27 14:31:40 +0200

    Add content view to splashactivity

--- app/src/main/java/com/lusivic/weatherhistory/ui/splashscreen/view/SplashActivity.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/splashscreen/view/SplashActivity.kt
@@ -2,13 +2,14 @@ package com.lusivic.weatherhistory.ui.splashscreen.view
 
 import android.content.Intent
 import android.os.Bundle
+import com.lusivic.weatherhistory.R
 import com.lusivic.weatherhistory.ui.base.view.BaseActivity
 import com.lusivic.weatherhistory.ui.main.MainActivity
 
 class SplashActivity : BaseActivity(), SplashMVPView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //MainActivity::class.start(this, true)
+        setContentView(R.layout.activity_splash)
     }
     override fun openMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
