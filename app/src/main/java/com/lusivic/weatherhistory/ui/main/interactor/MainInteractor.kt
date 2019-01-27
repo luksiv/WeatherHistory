d8b418bf8d6c93926f5554ee58d7fa0bee178a18
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-27 16:19:57 +0200

    Create MainInteractor
    It is only a mock

---
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/interactor/MainInteractor.kt
@@ -0,0 +1,32 @@
+package com.lusivic.weatherhistory.ui.main.interactor
+
+import com.lusivic.weatherhistory.data.db.weatherReport.WeatherReport
+import com.lusivic.weatherhistory.ui.base.interactor.BaseInteractor
+import java.util.*
+import kotlin.math.roundToInt
+
+class MainInteractor : BaseInteractor(), MainMVPInteractor {
+    override fun getCurrentWeatherReport(): WeatherReport {
+        val temperature = (Random().nextFloat() * 60 - 30*10).roundToInt().toFloat() / 10
+        val name: String = when {
+            temperature < -20 -> "Freezing"
+            temperature > -20 && temperature < -10 -> "Very cold"
+            temperature > -10 && temperature < 0 -> "Cold"
+            temperature in 1..9 -> "Chilly"
+            temperature in 11..19 -> "Warm"
+            temperature > 20 -> "Hot"
+            else -> "Don't know"
+        }
+
+        return WeatherReport(
+            0,
+            name,
+            "Description",
+            temperature,
+            80,
+            10.1f,
+            "Vilnius",
+            100020212
+        )
+    }
+}
