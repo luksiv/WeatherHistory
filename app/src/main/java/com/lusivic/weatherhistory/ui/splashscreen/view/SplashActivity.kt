79a081d26afacd1d5578d3b3443630073aa6c0cd
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-27 14:47:54 +0200

    Add a call to redirect from splash to main activity

--- app/src/main/java/com/lusivic/weatherhistory/ui/splashscreen/view/SplashActivity.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/splashscreen/view/SplashActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import com.lusivic.weatherhistory.R
 import com.lusivic.weatherhistory.ui.base.view.BaseActivity
-import com.lusivic.weatherhistory.ui.main.MainActivity
+import com.lusivic.weatherhistory.ui.main.view.MainActivity
 
 class SplashActivity : BaseActivity(), SplashMVPView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        openMainActivity()
     }
     override fun openMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
