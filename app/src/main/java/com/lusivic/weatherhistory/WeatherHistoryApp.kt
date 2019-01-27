44622c1d0e0541e5c79a09abc93a1ccbba03da89
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-27 14:36:43 +0200

    Remove non-existing imports and methods

--- app/src/main/java/com/lusivic/weatherhistory/WeatherHistoryApp.kt
+++ app/src/main/java/com/lusivic/weatherhistory/WeatherHistoryApp.kt
@@ -1,21 +1,10 @@
 package com.lusivic.weatherhistory
 
 import android.app.Application
-import com.lusivic.weatherhistory.di.AppComponent
-import com.lusivic.weatherhistory.di.AppModule
-import com.lusivic.weatherhistory.di.DaggerAppComponent
 
 class WeatherHistoryApp : Application() {
 
-    lateinit var appComponent: AppComponent
-
-    private fun initDagger(app: WeatherHistoryApp): AppComponent =
-        DaggerAppComponent.builder()
-            .appModule(AppModule(app))
-            .build()
-
     override fun onCreate() {
         super.onCreate()
-        appComponent = initDagger(this)
     }
 }
