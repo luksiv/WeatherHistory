721f89af5471f84d7cb7fbd1fae858b7c0c866dc
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-28 12:56:55 +0200

    Change name of Main screens interfaces

--- app/src/main/java/com/lusivic/weatherhistory/ui/main/presenter/IMainPresenter.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/presenter/IMainPresenter.kt
@@ -1,10 +1,10 @@
 package com.lusivic.weatherhistory.ui.main.presenter
 
 import com.lusivic.weatherhistory.ui.base.presenter.MVPPresenter
-import com.lusivic.weatherhistory.ui.main.interactor.MainMVPInteractor
+import com.lusivic.weatherhistory.ui.main.interactor.IMainInteractor
 import com.lusivic.weatherhistory.ui.main.view.MainMVPView
 
-interface MainMVPPresenter<V : MainMVPView, I : MainMVPInteractor> : MVPPresenter<V, I> {
+interface MainMVPPresenter<V : MainMVPView, I : IMainInteractor> : MVPPresenter<V, I> {
     fun getLatestWeatherReport(): Unit?
     fun onHistoryClick(): Unit?
     fun onRefreshClick(): Unit?
