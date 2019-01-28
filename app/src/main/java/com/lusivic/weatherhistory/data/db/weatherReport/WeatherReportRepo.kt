eb26b49323954a275b8fc9b443f18d2efbd8f0d9
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-28 19:37:01 +0200

    Update insertWeatherReport to Completable

--- app/src/main/java/com/lusivic/weatherhistory/data/db/weatherReport/WeatherReportRepo.kt
+++ app/src/main/java/com/lusivic/weatherhistory/data/db/weatherReport/WeatherReportRepo.kt
@@ -1,5 +1,6 @@
 package com.lusivic.weatherhistory.data.db.weatherReport
 
+import io.reactivex.Completable
 import io.reactivex.Single
 import javax.inject.Inject
 
@@ -8,10 +9,7 @@ class WeatherReportRepo @Inject internal constructor(private val weatherReportDa
     override fun isWeatherHistoryRepoEmpty(): Single<Boolean> =
         Single.fromCallable { weatherReportDao.selectAll().isEmpty() }
 
-    override fun insertWeatherReport(weatherReport: WeatherReport): Single<Boolean> {
-        weatherReportDao.insert(weatherReport)
-        return Single.just(true)
-    }
+    override fun insertWeatherReport(weatherReport: WeatherReport): Completable = Completable.fromCallable { weatherReportDao.insert(weatherReport) }
 
     override fun getAllWeatherReports(): Single<List<WeatherReport>> =
         Single.fromCallable { weatherReportDao.selectAll() }
