abdc1b48c636abbd33b0f1c9f1a3b1ec3dfcb6c4
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-28 19:32:51 +0200

    Add a new setUp() method

--- app/src/main/java/com/lusivic/weatherhistory/ui/history/view/IHistoryActivity.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/history/view/IHistoryActivity.kt
@@ -4,6 +4,7 @@ import com.lusivic.weatherhistory.data.db.weatherReport.WeatherReport
 import com.lusivic.weatherhistory.ui.base.view.MVPView
 
 interface IHistoryActivity : MVPView{
+    fun setUp()
     fun updateHistoryAdapter(history: List<WeatherReport>)
     fun setListEmpty()
     fun goBack()
