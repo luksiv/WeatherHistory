aad1df5a32b5fbeb358d7e033f961d5515839a94
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-28 14:36:41 +0200

    Implement IHistoryPresenter

---
+++ app/src/main/java/com/lusivic/weatherhistory/ui/history/presenter/HistoryPresenter.kt
@@ -0,0 +1,43 @@
+package com.lusivic.weatherhistory.ui.history.presenter
+
+import android.util.Log
+import com.lusivic.weatherhistory.ui.base.presenter.BasePresenter
+import com.lusivic.weatherhistory.ui.history.interactor.IHistoryInteractor
+import com.lusivic.weatherhistory.ui.history.view.IHistoryActivity
+import com.lusivic.weatherhistory.utils.SchedulerProvider
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class HistoryPresenter<V : IHistoryActivity, I : IHistoryInteractor> @Inject internal constructor(
+    interactor: I,
+    scheduler: SchedulerProvider,
+    disposable: CompositeDisposable
+) : BasePresenter<V, I>(interactor = interactor, schedulerProvider = scheduler, compositeDisposable = disposable),
+    IHistoryPresenter<V, I> {
+
+    override fun onAttach(view: V?) {
+        super.onAttach(view)
+        getWeatherReportHistory()
+    }
+
+    override fun getWeatherReportHistory() {
+        getView()?.showProgress()
+        interactor?.let {
+            compositeDisposable.add(
+                it.getWeatherReportHistory()
+                    .compose(schedulerProvider.ioToMainSingleScheduler())
+                    .subscribe({ weatherReportHistory ->
+                        getView()?.let {
+                            if(weatherReportHistory.isEmpty()) it.setListEmpty()
+                            else it.updateHistoryAdapter(weatherReportHistory)
+                            it.hideProgress()
+                        }
+                    }, {err -> Log.e("HistoryPresenter/getWeatherReportHistory", err.message)})
+            )
+        }
+    }
+
+    override fun onBackPressed() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
