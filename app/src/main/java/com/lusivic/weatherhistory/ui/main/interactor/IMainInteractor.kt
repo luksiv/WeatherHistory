b3dbe556b7a9ac2da0c6012c7530cc5588eb81f0
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-28 19:31:43 +0200

    Change return type of insertWeatherReport method

--- app/src/main/java/com/lusivic/weatherhistory/ui/main/interactor/IMainInteractor.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/interactor/IMainInteractor.kt
@@ -2,10 +2,11 @@ package com.lusivic.weatherhistory.ui.main.interactor
 
 import com.lusivic.weatherhistory.data.db.weatherReport.WeatherReport
 import com.lusivic.weatherhistory.ui.base.interactor.MVPInteractor
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface IMainInteractor : MVPInteractor {
 
     fun getCurrentWeatherReport(): Single<WeatherReport>
-    fun insertWeatherReport(weatherReport: WeatherReport)
+    fun insertWeatherReport(weatherReport: WeatherReport): Completable
 }
