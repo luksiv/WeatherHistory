cb599a1701f22d607b8005390e79dc8d3c1d6b22
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-28 19:36:20 +0200

    Change return type of insertWeatherReport to Completable

--- app/src/main/java/com/lusivic/weatherhistory/data/db/weatherReport/IWeatherReportRepo.kt
+++ app/src/main/java/com/lusivic/weatherhistory/data/db/weatherReport/IWeatherReportRepo.kt
@@ -1,11 +1,12 @@
 package com.lusivic.weatherhistory.data.db.weatherReport
 
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface IWeatherReportRepo {
 
     fun isWeatherHistoryRepoEmpty(): Single<Boolean>
-    fun insertWeatherReport(weatherReport: WeatherReport): Single<Boolean>
+    fun insertWeatherReport(weatherReport: WeatherReport): Completable
     fun deleteWeatherReport(weatherReport: WeatherReport): Single<Boolean>
     fun getAllWeatherReports(): Single<List<WeatherReport>>
 
