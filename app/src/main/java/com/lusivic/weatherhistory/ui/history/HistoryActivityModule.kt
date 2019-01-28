831d481523eddd20a216bb2eeaf3aebf0405dc6d
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-28 14:35:56 +0200

    Add provide methods for history module

---
+++ app/src/main/java/com/lusivic/weatherhistory/ui/history/HistoryActivityModule.kt
@@ -0,0 +1,20 @@
+package com.lusivic.weatherhistory.ui.history
+
+import com.lusivic.weatherhistory.ui.history.interactor.HistoryInteractor
+import com.lusivic.weatherhistory.ui.history.interactor.IHistoryInteractor
+import com.lusivic.weatherhistory.ui.history.presenter.HistoryPresenter
+import com.lusivic.weatherhistory.ui.history.presenter.IHistoryPresenter
+import com.lusivic.weatherhistory.ui.history.view.IHistoryActivity
+import dagger.Module
+import dagger.Provides
+
+@Module
+class HistoryActivityModule {
+
+    @Provides
+    internal fun provideHistoryInteractor(historyInteractor: HistoryInteractor): IHistoryInteractor = historyInteractor
+
+    @Provides
+    internal fun provideHistoryPresenter(historyPresenter: HistoryPresenter<IHistoryActivity, IHistoryInteractor>):
+            IHistoryPresenter<IHistoryActivity, IHistoryInteractor> = historyPresenter
+}
