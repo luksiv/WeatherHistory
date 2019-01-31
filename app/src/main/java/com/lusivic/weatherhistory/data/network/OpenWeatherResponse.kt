0498aefcc4bec19532840f547ba8b4f52a5b5b59
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-31 21:05:46 +0200

    Create data classes for parsing OpenWeatherMapApi response

---
+++ app/src/main/java/com/lusivic/weatherhistory/data/network/OpenWeatherResponse.kt
@@ -0,0 +1,88 @@
+package com.lusivic.weatherhistory.data.network
+
+import com.google.gson.annotations.SerializedName
+
+data class OpenWeatherResponse(
+    @SerializedName("base")
+    val base: String,
+    @SerializedName("clouds")
+    val clouds: Clouds,
+    @SerializedName("cod")
+    val cod: Int,
+    @SerializedName("coord")
+    val coord: Coord,
+    @SerializedName("dt")
+    val dt: Int,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("main")
+    val main: Main,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("sys")
+    val sys: Sys,
+    @SerializedName("visibility")
+    val visibility: Int,
+    @SerializedName("weather")
+    val weather: List<Weather>,
+    @SerializedName("wind")
+    val wind: Wind
+) {
+    data class Coord(
+        @SerializedName("lat")
+        val lat: Double,
+        @SerializedName("lon")
+        val lon: Double
+    )
+
+    data class Sys(
+        @SerializedName("country")
+        val country: String,
+        @SerializedName("id")
+        val id: Int,
+        @SerializedName("message")
+        val message: Double,
+        @SerializedName("sunrise")
+        val sunrise: Int,
+        @SerializedName("sunset")
+        val sunset: Int,
+        @SerializedName("type")
+        val type: Int
+    )
+
+    data class Weather(
+        @SerializedName("description")
+        val description: String,
+        @SerializedName("icon")
+        val icon: String,
+        @SerializedName("id")
+        val id: Int,
+        @SerializedName("main")
+        val main: String
+    )
+
+    data class Main(
+        @SerializedName("humidity")
+        val humidity: Int,
+        @SerializedName("pressure")
+        val pressure: Int,
+        @SerializedName("temp")
+        val temp: Double,
+        @SerializedName("temp_max")
+        val tempMax: Double,
+        @SerializedName("temp_min")
+        val tempMin: Double
+    )
+
+    data class Clouds(
+        @SerializedName("all")
+        val all: Int
+    )
+
+    data class Wind(
+        @SerializedName("deg")
+        val deg: Int,
+        @SerializedName("speed")
+        val speed: Double
+    )
+}
