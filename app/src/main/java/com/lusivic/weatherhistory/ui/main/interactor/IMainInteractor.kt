1d5618e8709428ec1ef41284c871c4be9183e992
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-31 21:05:04 +0200

    Change return type and parameters for getCurrentWeatherReport

--- app/src/main/java/com/lusivic/weatherhistory/ui/main/interactor/IMainInteractor.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/interactor/IMainInteractor.kt
@@ -1,12 +1,13 @@
 package com.lusivic.weatherhistory.ui.main.interactor
 
 import com.lusivic.weatherhistory.data.db.weatherReport.WeatherReport
+import com.lusivic.weatherhistory.data.network.OpenWeatherResponse
 import com.lusivic.weatherhistory.ui.base.interactor.MVPInteractor
 import io.reactivex.Completable
 import io.reactivex.Single
 
 interface IMainInteractor : MVPInteractor {
 
-    fun getCurrentWeatherReport(): Single<WeatherReport>
+    fun getCurrentWeatherReport(latitude: Float, longitude: Float): Single<OpenWeatherResponse>
     fun insertWeatherReport(weatherReport: WeatherReport): Completable
 }
