5c61d8be7fdda1a855a5fa5f39faaf404d4145f4
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-31 21:00:41 +0200

    Add base open weather map api url

--- app/src/main/java/com/lusivic/weatherhistory/utils/AppConstants.kt
+++ app/src/main/java/com/lusivic/weatherhistory/utils/AppConstants.kt
@@ -5,4 +5,6 @@ object AppConstants {
     internal val APP_DB_NAME = "luksivic_weather.db"
 
     internal val LOCATION_REQUEST_CODE = 1001
+
+    internal val BASE_OW_API_URL = "https://api.openweathermap.org/"
 }
