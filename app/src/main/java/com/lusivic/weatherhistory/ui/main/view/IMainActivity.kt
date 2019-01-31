4aebde1c9a243f23626d97cd267529fc34effed3
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-31 16:41:11 +0200

    Add methods for location permission

--- app/src/main/java/com/lusivic/weatherhistory/ui/main/view/IMainActivity.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/view/IMainActivity.kt
@@ -4,8 +4,13 @@ import com.lusivic.weatherhistory.data.db.weatherReport.WeatherReport
 import com.lusivic.weatherhistory.ui.base.view.MVPView
 
 interface IMainActivity : MVPView {
+    fun setUp()
     fun showCurrentWeather(weather: WeatherReport)
     fun openHistoryActivity()
     fun showInsertSuccessMessage()
     fun showInsertFailedMessage()
+    fun showPermissionDenied()
+    fun isLocationPermissionGranted(): Boolean
+    fun requestLocationPermission()
+
 }
