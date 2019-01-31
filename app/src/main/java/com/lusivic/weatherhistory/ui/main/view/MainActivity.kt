5da83431b4b2d49c842344fc49377c0338745381
Lukas Šivickas <l.sivickas@gmail.com>
2019-01-31 16:40:48 +0200

    Add fine location permission request

--- app/src/main/java/com/lusivic/weatherhistory/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/lusivic/weatherhistory/ui/main/view/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.lusivic.weatherhistory.ui.main.view
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.lusivic.weatherhistory.R
 import com.lusivic.weatherhistory.data.db.weatherReport.WeatherReport
 import com.lusivic.weatherhistory.ui.base.view.BaseActivity
 import com.lusivic.weatherhistory.ui.history.view.HistoryActivity
 import com.lusivic.weatherhistory.ui.main.interactor.IMainInteractor
 import com.lusivic.weatherhistory.ui.main.presenter.IMainPresenter
+import com.lusivic.weatherhistory.utils.AppConstants
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -26,7 +31,43 @@ class MainActivity : BaseActivity(), IMainActivity {
         setContentView(R.layout.activity_main)
         AndroidInjection.inject(this)
         mPresenter.onAttach(this)
+    }
+
+    override fun setUp() {
         setOnClickListeners()
+        mPresenter.onViewReady()
+    }
+
+    override fun isLocationPermissionGranted(): Boolean = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    override fun requestLocationPermission() = ActivityCompat.requestPermissions(
+        this,
+        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+        AppConstants.LOCATION_REQUEST_CODE
+    )
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            AppConstants.LOCATION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission granted
+                    mPresenter.onLocationPermissionGranted()
+                } else {
+                    // permission denied
+                    mPresenter.onLocationPermissionDenied()
+                }
+            }
+            else -> {
+                // ignore other requests
+            }
+        }
+    }
+
+    override fun showPermissionDenied() {
+        Toast.makeText(this, "Location permission is used for the app to work", Toast.LENGTH_LONG).show()
     }
 
     override fun showCurrentWeather(weather: WeatherReport) {
